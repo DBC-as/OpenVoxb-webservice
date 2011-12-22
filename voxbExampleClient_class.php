7a94a7fec07bc197ec3446d9c482f11ab444cfe7
mkr <mkr@30e9dc4a-7611-4fbc-83ba-eecf79b76bd0>
2011-12-22 12:35:10 +0000

    Display now works...
    git-svn-id: svn://oss.dbc.dk/sync_php/php/OpenLibrary/OpenVoxb/trunk@36414 30e9dc4a-7611-4fbc-83ba-eecf79b76bd0

--- voxbExampleClient_class.php
+++ voxbExampleClient_class.php
@@ -37,14 +37,64 @@ class voxbExampleClient extends webServiceClientUtils {
 		return $this->send_request($rn, $this->request_action);
 	}
 
-	function displayData() {
-		echo $this->header();
+	function extractData($obj, &$data) {
+   foreach ($obj as $k=>$v) {
+			if(is_string($k) && ($k=="aliasName" || $k=="averageRating" || $k=="rating" || $k=="reviewData" || $k=="reviewTitle")) {
+        $data[$k][]=$v->_value;
+      }
+      if(is_object($v) || is_array($v)) {
+        $this->extractData($v, $data);
+      }
+    }
+		return $data;
+	}
+
+	function makeDisplayData($data) {
+		if(is_array($data["rating"]))
+			array_shift($data["rating"]);
+		foreach($data["aliasName"] as $k=>$v) {
+			$displaydata[$k]["aliasName"]=$v;
+			$displaydata[$k]["rating"]=$data["rating"][$k];
+			$displaydata[$k]["reviewTitle"]=$data["reviewTitle"][$k];
+			$displaydata[$k]["reviewData"]=$data["reviewData"][$k];
+			if(
+				empty($data["rating"][$k]) && empty($data["reviewTitle"][$k]) && empty($data["reviewData"][$k]) ) {
+				unset($displaydata[$k]);
+			} 
+		}
+		return($displaydata);
+	}
+
+	function displayData($obj) {
+		echo "<pre>";
+		$data=array();
+		$data=$this->extractData($obj, $data);
+		$displaydata=$this->makeDisplayData($data);
 		
-		echo $this->footer();
+		foreach($displaydata as $k=>$v) {
+			echo $v["aliasName"];
+			echo "<br>";
+			if(!empty($v["rating"])) {
+				echo "rating: ";
+				echo $v["rating"];
+				echo "<br>";
+			}
+			if(!empty($v["reviewTitle"])) {
+				echo "review titel: ";
+				echo $v["reviewTitle"];
+				echo "<br>";
+			}
+			if(!empty($v["reviewData"])) {
+				echo "review: ";
+				echo $v["reviewData"];
+				echo "<br>";
+			}
+			echo "<hr>";
+		
+		}
 	}
 	
 	function displayFetchForm() {
-		echo $this->header();
 		echo "<FORM METHOD='POST'>";
 		echo "objectIdentifierType: <SELECT NAME='objectIdentifierType'>\n";
 		echo "<OPTION VALUE='ISBN'>ISBN</OPTION>\n";
@@ -53,7 +103,6 @@ class voxbExampleClient extends webServiceClientUtils {
 		echo "<br>objectIdentifierValue: <INPUT TYPE='TEXT' NAME='objectIdentifierValue'>\n";
 		echo "<input type='SUBMIT' NAME='fetchDataRequest' value='fetchDataRequest'>";
 		echo "</FORM>";
-		echo $this->footer();
 	}
 
 	function header() {
@@ -66,25 +115,27 @@ class voxbExampleClient extends webServiceClientUtils {
 
 }
 
-
-$vEC=new voxbExampleClient("xml/request/");
-$vEC->set_request_action("http://metode.dbc.dk/~mkr/OpenVoxb/trunk/");
-//$xml=$vEC->createMyDataRequest(275,100,array('A', 'B'),"Forfatter", 11111111111111, "ISBN", "Bog", 1900, "titel");
-//echo $vEC->check_error($vEC->xmlconvert->soap2obj($xml));
-//echo $vEC->fetchDataRequest("11111111111111", "ISBN", "DK-100450", 790900);
-$vEC->displayFetchForm();
+$client=new voxbExampleClient("xml/request/");
+echo $client->header();
+$client->set_request_action("http://metode.dbc.dk/~mkr/OpenVoxb/trunk/");
+//$xml=$client->createMyDataRequest(275,100,array('A', 'B'),"Forfatter", 11111111111111, "ISBN", "Bog", 1900, "titel");
+//echo $client->check_error($client->xmlconvert->soap2obj($xml));
+//echo $client->fetchDataRequest("11111111111111", "ISBN", "DK-100450", 790900);
+$client->displayFetchForm();
 
 if(isset($_POST["fetchDataRequest"])) {
-	$xml=$vEC->fetchDataRequest($_POST["objectIdentifierValue"], $_POST["objectIdentifierType"]);
- 	if($vEC->check_error($vEC->xmlconvert->soap2obj($xml))) {
+	$xml=$client->fetchDataRequest($_POST["objectIdentifierValue"], $_POST["objectIdentifierType"]);
+	$obj=$client->xmlconvert->soap2obj($xml);
+ 	if($client->check_error($obj)) {
 		echo "Could not find any items...";
 	} else {
-		echo $xml;
+		$client->displayData($obj);
 	}
 }
 
 
 //$obj->Envelope->_value->Body->_value->createMyDataRequest->_value->userId
 
+echo $client->footer();
 
 ?>

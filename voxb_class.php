4fd978b213cd14352ead2fbf3180f289e3e87b32
mkr <mkr@30e9dc4a-7611-4fbc-83ba-eecf79b76bd0>
2012-09-13 13:25:05 +0000

    Activated isbn et al cleaning code
    git-svn-id: svn://oss.dbc.dk/sync_php/php/OpenLibrary/OpenVoxb/trunk@47393 30e9dc4a-7611-4fbc-83ba-eecf79b76bd0

--- voxb_class.php
+++ voxb_class.php
@@ -737,8 +737,6 @@ class voxb extends webServiceServer {
 				$objectIdentifierValue=$v->_value->objectIdentifierValue->_value;
 
 
-				// Switch SKAL MAASKE PILLES UD NAAR OPENXID ER I DRIFT
-				/*
 				switch($v->_value->objectIdentifierType->_value) {
 					case "ISBN":
 						$objectIdentifierValue=materialId::normalizeISBN($objectIdentifierValue);
@@ -754,8 +752,6 @@ class voxb extends webServiceServer {
 						$objectIdentifierValue=materialId::normalizeFAUST($objectIdentifierValue);
 					break;
 				}
-				*/
-				// Switch SKAL MAASKE PILLES UD NAAR OPENXID ER I DRIFT
 
           // Requested data element is an object
           $olist[] = "(OBJECTIDENTIFIERVALUE='" . $objectIdentifierValue . "' AND OBJECTIDENTIFIERTYPE='" . $v->_value->objectIdentifierType->_value . "')";
